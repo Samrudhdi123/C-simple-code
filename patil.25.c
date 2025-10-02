760d0093371c9764582b0afe5b88ce699203f561
Samrudhdi Patil <psamrudhdi@gmail.com>
2025-10-02 22:39:59 +0530

    Create patil.25.c

---
+++ patil.25.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    char name[50];
+    int a,b,diff;
+  
+  printf("Enter your name: "); scanf("%s", name);
+   
+ printf("Enter two numbers: "); scanf("%d%d", &a,&b);
+    diff = b - a;
+   
+ printf("Hello %s, difference is %d
+", name, diff);
+    return 0;
+}
