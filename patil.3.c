b7ce57c2862b5a8b250c3a8298f21a8e17ff69f9
Samrudhdi Patil <psamrudhdi@gmail.com>
2025-10-02 21:51:38 +0530

    Create patil.3.c

---
+++ patil.3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char name[50]; 
+
+    printf("Please enter your name: ");
+    scanf("%s", name);
+
+    printf("\nHello, Robotics! My name is %s.\n", name);
+
+    return 0;
+}
