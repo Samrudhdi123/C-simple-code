0f1bd8db8820e92684e56f5a8827a89c150629c3
Samrudhdi Patil <psamrudhdi@gmail.com>
2025-10-02 22:48:24 +0530

    Create patil.45.c

---
+++ patil.45.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+    char name[50];
+    int a,b,result;
+  
+  printf("Enter your name: ");
+    scanf("%s", name);
+ 
+   printf("Enter two numbers: ");
+    scanf("%d%d", &a,&b);
+    result = (a + b) * 2;
+  
+  printf("Hello %s, (sum * 2) = %d
+", name, result);
+    return 0;
+}
